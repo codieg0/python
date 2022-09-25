df2aced1d48dc17536cfcc2d99b8562be5f6edef
Diego Castro <castroosorio.diego@gmail.com>
2022-09-25 10:27:34 +0100

    list

---
+++ list.py
@@ -0,0 +1,13 @@
+'''
+Take a list, say for example this one:
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+and write a program that prints out all the elements of the list that are less than 5.
+'''
+
+list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+for num in list:
+  if num < 5: print(num)
+  # print(num)
+
+# Need to get the number in the list
