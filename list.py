af0d739e02e21a94d42c5114fe6f2c3fc99a87aa
Diego Castro <castroosorio.diego@gmail.com>
2022-09-25 10:30:43 +0100

    list.py 2nd version

--- list.py
+++ list.py
@@ -7,7 +7,8 @@ and write a program that prints out all the elements of the list that are less t
 list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 for num in list:
-  if num < 5: print(num)
+  if num < 5: 
+    print(num)
   # print(num)
 
 # Need to get the number in the list
