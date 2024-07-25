021b89eaed5b7edcda49d0c85888640200b860b7
Diego C <63706443+codieg0@users.noreply.github.com>
2024-07-25 11:47:03 +0100

    New directories

---
+++ learning/Exercises_Day_1.py
@@ -0,0 +1,83 @@
+#!/bin/usr/env python
+
+'''
+Exercise: Level 1
+
+1. Check the python version you are using
+$ python3 --version
+Python 3.12.3
+
+2. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+    - addition(+)
+    - subtraction(-)
+    - multiplication(*)
+    - modulus(%)
+    - division(/)
+    - exponential(**)
+    - floor division operator(//)
+
+>>> 2+3
+5
+>>> 3-1
+2
+>>> 2*3
+6
+>>> 2%7
+2
+>>> 2/4
+0.5
+>>> 4**4
+256
+>>> 5 // 2
+2
+
+3. Write strings on the python interactive shell. The strings are the following:
+    - Your name
+    - Your family name
+    - Your country
+    - I am enjoying 30 days of python
+>>> name = 'Diego'
+>>> familyName = 'Castro'
+>>> country = 'Ireland'
+>>> Enjoying30DaysofPython = True
+>>> print(name, familyName, country, Enjoying30DaysofPython)
+Diego Castro Ireland True
+
+4. Check the data types of the following data:
+    - 10
+    - 9.8
+    - 3.14
+    - 4 - 4j
+    - ['Asabeneh', 'Python', 'Finland']
+    - Your name
+    - Your family name
+    - Your country
+>>> type(10)
+<class 'int'>
+>>> type(9.8)
+<class 'float'>
+>>> type(3.14)
+<class 'float'>
+>>> type(['Asabeneh', 'Python', 'Finland'])
+<class 'list'>
+>>> type(name)
+<class 'str'>
+>>> type(familyName)
+<class 'str'>
+>>> type(country)
+<class 'str'>
+>>> type(Enjoying30DaysofPython)
+<class 'bool'>
+
+
+Exercise: Level 2
+
+Create a folder named day_1 inside 30DaysOfPython folder. Inside day_1 folder, create a python file helloworld.py and repeat questions 1, 2, 3 and 4. Remember to use print() when you are working on a python file. Navigate to the directory where you have saved your file, and run it.
+
+Done in the previous exercise
+
+Exercise: Level 3
+
+Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+Find an Euclidian distance between (2, 3) and (10, 8)
+'''
